476b257fc58ecf86d9beaedd7b67a23e061b2ac8
ninalamo <nin.alamo@outlook.com>
2026-09-05 15:04:51 +0800

    feat: seed default organization in DatabaseSeeder

--- assemblies/loa-cert-platform/database/seeders/DatabaseSeeder.php
+++ assemblies/loa-cert-platform/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Organization;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        Organization::updateOrCreate(
+            ['id' => '00000000-0000-0000-0000-000000000001'],
+            ['name' => 'LOA E-Cert Platform', 'slug' => 'loa-e-cert']
+        );
     }
 }
